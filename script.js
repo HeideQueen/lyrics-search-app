aac7859f083dc4b2f1dbfb30ebec425491033d38
HeideQueen <lucyfabz@gmail.com>
2020-07-08 01:14:59 -0500

    get & display song lyrics

--- script.js
+++ script.js
@@ -64,6 +64,22 @@ async function getMoreSongs(url) {
   }
 }
 
+// get lyrics for song
+async function getLyrics(artist, songTitle) {
+  try {
+    const res = await fetch(`${apiURL}/v1/${artist}/${songTitle}`);
+    const data = await res.json();
+
+    const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>');
+
+    result.innerHTML = `<h2><strong>${artist}</strong> - ${songTitle}</h2><span>${lyrics}</span>`;
+
+    more.innerHTML = '';
+  } catch (err) {
+    console.log(err);
+  }
+}
+
 // Event listeners
 form.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -74,5 +90,17 @@ form.addEventListener('submit', (e) => {
     alert('please type in a search term');
   } else {
     searchSongs(searchTerm);
+  }
+});
+
+// get lyrics button click
+result.addEventListener('click', (e) => {
+  const clickedEl = e.target;
+
+  if (clickedEl.tagName === 'BUTTON') {
+    const artist = clickedEl.getAttribute('data-artist');
+    const songTitle = clickedEl.getAttribute('data-songtitle');
+
+    getLyrics(artist, songTitle);
   }
 });
